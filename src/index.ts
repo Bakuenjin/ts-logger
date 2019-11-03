2f59d614073ec5b54c36cef9f6f7fe2b0337149c
Julian Zameit <julian.zameit.96@gmail.com>
2019-11-03 14:34:11 +0100

    index exposing the logger and needed interfaces

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Logger from "./models/Logger"
+import ILogFormatter from "./models/ILogFormatter"
+import ILogInformation from "./models/LogInformation/meta/ILogInformation"
+import ILogInformationFactory from "./models/LogInformation/meta/ILogInformationFactory"
+
+export { Logger, ILogFormatter, ILogInformation, ILogInformationFactory }
